d0f9883fdb8689c1dcf73fd971f0451864188277
Joshua Kolodny <kolo.josh@gmail.com>
2023-09-02 18:26:48 +0000

    basic code for generating hits

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Fake_Baseball_Data_Simulation
+
+## Motivation
+
+* Lately, when trying to work on sports analytics projects, I realize that a big
+bottleneck (at least for me) is getting my hands on data. In effect, this makes it so that when I want to start a project, I try to re-learn a little about web scraping for a day or two, and then quit soon after that. 
+
+* The value I'm trying to create here is the ability to generate simple, probably not great, but reasonable fake data for use in projects before committing to developing web scraping pipelines.
---
+++ scripts/generator.py
@@ -0,0 +1,27 @@
+import json
+import numpy as np
+
+# importing assumptions
+with open('../assumptions.json', 'r', encoding='utf-8') as f:
+    assumptions = json.load(f)
+
+# assumptions
+RANDOM_SEED = assumptions["RANDOM_SEED"]
+CAREER_LENGTH = assumptions["CAREER_LENGTH"]
+NUMBER_GAMES_SEASON = assumptions[ "NUMBER_GAMES_SEASON"]
+
+# seed
+np.random.seed(RANDOM_SEED)
+
+CAREER_STATS = []
+for _ in range(CAREER_LENGTH):
+    SEASON_STATS = 0
+    for _ in range(NUMBER_GAMES_SEASON):
+        AT_BATS_IN_GAME = np.random.randint(1, 6)
+        OUTCOME = np.random.randint(0, AT_BATS_IN_GAME)
+        SEASON_STATS += OUTCOME
+
+    CAREER_STATS.append(SEASON_STATS)
+
+print(CAREER_STATS)
+print(sum(CAREER_STATS))
