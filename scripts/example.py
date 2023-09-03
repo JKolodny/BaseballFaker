4886f158b964733921d3d1ef98bab45769208833
Joshua Kolodny <kolo.josh@gmail.com>
2023-09-03 10:20:28 -0400

    test

---
+++ scripts/example.py
@@ -0,0 +1,6 @@
+from generator import generation
+
+test = generation(CAREER_LENGTH=2, 
+                  NUMBER_GAMES_SEASON=30)
+
+print(test)
--- scripts/generator.py
+++ scripts/generator.py
@@ -1,69 +1,70 @@
 """ Generating Fake Baseball Data """
-import json
+
+# import libraries
 import numpy as np
 from faker import Faker
-fake = Faker()
 
-# importing assumptions
-with open("../assumptions.json", "r", encoding="utf-8") as f:
-    assumptions = json.load(f)
+def generation(SEED = 42, 
+               CAREER_LENGTH = 20,
+                NUMBER_GAMES_SEASON = 162 ):
 
-# assumptions
-RANDOM_SEED = assumptions["RANDOM_SEED"]
-CAREER_LENGTH = assumptions["CAREER_LENGTH"]
-NUMBER_GAMES_SEASON = assumptions["NUMBER_GAMES_SEASON"]
+    """
+    Generates Fake Baseball Career Data
+    """
 
-# variable career length
-CAREER_MULTIPLIER = np.random.uniform(0.7, 1.2)
-CAREER_LENGTH = round(CAREER_LENGTH * CAREER_MULTIPLIER)
+    fake = Faker()
 
-# seed
-np.random.seed(RANDOM_SEED)
+    # variable career length
+    CAREER_MULTIPLIER = np.random.uniform(0.7, 1.2)
+    CAREER_LENGTH = round(CAREER_LENGTH * CAREER_MULTIPLIER)
 
-# generating name
-# Generate a random male first name
-first_name = fake.first_name_male()
+    # seed
+    np.random.seed(SEED)
 
-# Generate a random last name
-last_name = fake.last_name()
+    # generating name
+    # Generate a random male first name
+    first_name = fake.first_name_male()
 
-CAREER_STATS = {"Hits": [],
-                "AB": [], 
-                "AVG": [], 
-                "2B": [], 
-                "3B": [], 
-                "HR": [],
-                "Name":[f"{first_name} {last_name}"] * CAREER_LENGTH}
+    # Generate a random last name
+    last_name = fake.last_name()
 
-for _ in range(CAREER_LENGTH):
-    HITS = 0
-    AB = 0
-    _2B = 0
-    _3B = 0
-    HR = 0
-    for _ in range(NUMBER_GAMES_SEASON):
-        AT_BATS_IN_GAME = np.random.randint(1, 6)
-        OUTCOME = np.random.randint(0, AT_BATS_IN_GAME)
-        HITS += OUTCOME
-        AB += AT_BATS_IN_GAME
+    CAREER_STATS = {"Hits": [],
+                    "AB": [], 
+                    "AVG": [], 
+                    "2B": [], 
+                    "3B": [], 
+                    "HR": [],
+                    "Name":[f"{first_name} {last_name}"] * CAREER_LENGTH}
 
-        for _ in range(HITS):
-            HIT_TYPE = np.random.randint(0, 22)
-            if HIT_TYPE < 16:
-                break
-            elif HIT_TYPE < 20:
-                _2B += 1
-            elif HIT_TYPE < 21:
-                _3B += 1
-            else:
-                HR += 1
+    for _ in range(CAREER_LENGTH):
+        HITS = 0
+        AB = 0
+        _2B = 0
+        _3B = 0
+        HR = 0
+        for _ in range(NUMBER_GAMES_SEASON):
+            AT_BATS_IN_GAME = np.random.randint(1, 6)
+            OUTCOME = np.random.randint(0, AT_BATS_IN_GAME)
+            HITS += OUTCOME
+            AB += AT_BATS_IN_GAME
+
+            for _ in range(HITS):
+                HIT_TYPE = np.random.randint(0, 22)
+                if HIT_TYPE < 16:
+                    break
+                elif HIT_TYPE < 20:
+                    _2B += 1
+                elif HIT_TYPE < 21:
+                    _3B += 1
+                else:
+                    HR += 1
 
 
-    CAREER_STATS["Hits"].append(HITS)
-    CAREER_STATS["AB"].append(AB)
-    CAREER_STATS["2B"].append(_2B)
-    CAREER_STATS["3B"].append(_3B)
-    CAREER_STATS["HR"].append(HR)
-    CAREER_STATS["AVG"].append(round(HITS / AB, 3))
+        CAREER_STATS["Hits"].append(HITS)
+        CAREER_STATS["AB"].append(AB)
+        CAREER_STATS["2B"].append(_2B)
+        CAREER_STATS["3B"].append(_3B)
+        CAREER_STATS["HR"].append(HR)
+        CAREER_STATS["AVG"].append(round(HITS / AB, 3))
 
-print(CAREER_STATS)
+    return CAREER_STATS
